3455aa0aa82702223fac29ce3e6a2d45b1963171
Diego Arenas <dtarenas@gmail.com>
2022-01-05 21:17:30 -0500

    Add conditional to render sections

---
+++ Blazor-Pelis-WebAssembly/Shared/Core/Dtos/Movies/MovieDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor_Pelis_WebAssembly.Shared.Core.Dtos.Movies
+{
+    public class MovieDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ Blazor-Pelis-WebAssembly/Shared/Utils/StringUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor_Pelis_WebAssembly.Shared.Utils
+{
+    public class StringUtils
+    {
+        public static string GetUpperCase(string str) => str.ToUpper();
+    }
+}
